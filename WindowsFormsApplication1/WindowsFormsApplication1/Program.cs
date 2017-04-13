c28d603db68f0bd560c04a212ce3a4c55ae032d1
pradeepbeeram <beerampradeep1991@gmail.com>
2017-04-13 00:20:56 -0700

    Update Program.cs

--- WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            Application.EnableVisualStyles();//for testing we just modified
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FilterSelection());
         }
